a2dac0440782d6370e508c8b658cab4f91098b95
Frechdachs <Frechdachs@users.noreply.github.com>
2016-07-22 19:11:17 +0200

    Dirty fix for my own problems #2

--- i3pystatus/gpu_temp.py
+++ i3pystatus/gpu_temp.py
@@ -24,14 +24,21 @@ class GPUTemperature(IntervalModule):
     color = "#FFFFFF"
     alert_temp = 90
     alert_color = "#FF0000"
-    display_if = True
 
     def run(self):
-        temp = gpu.query_nvidia_smi().temp
+        gpu_query = gpu.query_nvidia_smi()
+        temp = gpu_query.temp
+        percent_fan = gpu_query.percent_fan
+        total_mem = gpu_query.total_mem
+        avail_mem = gpu_query.avail_mem
+        used_mem = gpu_query.used_mem
+        usage_gpu = gpu_query.usage_gpu
+        usage_mem = gpu_query.usage_mem
         temp_alert = temp is None or temp >= self.alert_temp
 
-        if eval(self.display_if):
-            self.output = {
-                "full_text": self.format.format(temp=temp),
-                "color": self.color if not temp_alert else self.alert_color,
-            }
+        self.output = {
+            "full_text": self.format.format(temp=temp, percent_fan=percent_fan, total_mem=total_mem,
+                                            avail_mem=avail_mem, used_mem=used_mem,
+                                            usage_gpu=usage_gpu, usage_mem=usage_mem),
+            "color": self.color if not temp_alert else self.alert_color,
+        }
